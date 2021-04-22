6b6236acbc4fcd29757a8b8e09246a00839e09b9
AthanatiusC <lexi_anugrah@Hotmail.com>
2021-04-22 15:15:06 +0700

    Deploy Ready

--- models/models.go
+++ models/models.go
@@ -11,6 +11,11 @@ import (
 
 var db *mongo.Database
 
+// username usera
+// password b8VEuQAyiVocR8RQ
+
+// username client
+// password H76dQCR4DZ8bhBLI
 func init() {
 
 	e := godotenv.Load() //Load .env file
@@ -18,7 +23,8 @@ func init() {
 		fmt.Print(e)
 	}
 
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb+srv://client:H76dQCR4DZ8bhBLI@finagecluster0.lv8aj.mongodb.net/FinanceManager?retryWrites=true&w=majority")
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
@@ -26,6 +32,14 @@ func init() {
 		panic(err)
 	}
 	db = client.Database("FinanceManager")
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
 }
 
 func GetDB(whatDB string) *mongo.Database {
